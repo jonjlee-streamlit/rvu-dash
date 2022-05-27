44237217a759ac305edef8ade0dc4a75e21908f3
jonjlee <jonjlee@gmail.com>
2022-05-26 23:44:21 -0700

    Move date math into separate module, rearrange graphs

--- src/fig.py
+++ src/fig.py
@@ -2,16 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-def st_summary(stats, dates_ct, ct1, ct2, ct3):
+def st_summary(stats, start_date, end_date, dates_ct, ct1, ct2, ct3):
     """Render summary stats"""
     dates_ct.write(
-        f"Found data {stats['start_date'].strftime('%a %b %d, %Y')} to {stats['end_date'].strftime('%a %b %d, %Y')}",
+        f"{start_date.strftime('%a %b %d, %Y')} to {end_date.strftime('%a %b %d, %Y')}",
     )
     ct1.metric("Encounters", stats["ttl_encs"])
     ct2.metric("Total wRVU", round(stats["ttl_wrvu"]))
     ct3.metric("wRVU / encounter", round(stats["wrvu_per_encs"], 2))
 
-def st_summary_figs(df, partitions, main_ct, quarter_ct, daily_ct):
+def st_summary_figs(df, partitions, enc_ct, rvu_ct, quarter_enc_ct, quarter_rvu_ct, daily_enc_ct, daily_rvu_ct):
     # Number of visits
     encounters_by_quarter_src = partitions["all_encs"].groupby("quarter").mrn.count().reset_index()
     encounters_by_quarter_src.columns = ["Quarter", "Encounters"]
@@ -29,30 +29,30 @@ def st_summary_figs(df, partitions, main_ct, quarter_ct, daily_ct):
     # clinic does, so number match and the user knows what to expect at when comparing to the production report.
     # However, for wRVU/day, we showing it with the actual visit date, which is more helpful for understanding
     # actual production.
-    wrvu_by_month_src = df.groupby('posted_month').wrvu.sum().reset_index()
-    wrvu_by_month_src.columns = ['Month', 'wRVUs']
-    wrvu_by_month_fig = px.bar(wrvu_by_month_src, x='Month', y='wRVUs', text='wRVUs', text_auto='.1f', hover_data={'wRVUs': ':.1f'})
+    wrvu_by_month_src = df.groupby("posted_month").wrvu.sum().reset_index()
+    wrvu_by_month_src.columns = ["Month", "wRVUs"]
+    wrvu_by_month_fig = px.bar(wrvu_by_month_src, x="Month", y="wRVUs", text="wRVUs", text_auto=".1f", hover_data={"wRVUs": ":.1f"}).update_traces(marker_color="#2ca02c")
 
-    wrvu_by_quarter_src = df.groupby('posted_quarter').wrvu.sum().reset_index()
-    wrvu_by_quarter_src.columns = ['Quarter', 'wRVUs']
-    wrvu_by_quarter_fig = px.bar(wrvu_by_quarter_src, x='Quarter', y='wRVUs', text='wRVUs', text_auto='.1f', hover_data={'wRVUs': ':.1f'})
+    wrvu_by_quarter_src = df.groupby("posted_quarter").wrvu.sum().reset_index()
+    wrvu_by_quarter_src.columns = ["Quarter", "wRVUs"]
+    wrvu_by_quarter_fig = px.bar(wrvu_by_quarter_src, x="Quarter", y="wRVUs", text="wRVUs", text_auto=".1f", hover_data={"wRVUs": ":.1f"}).update_traces(marker_color="#2ca02c")
 
-    wrvu_by_day_src = df.groupby('date').wrvu.sum().reset_index()
-    wrvu_by_day_src.columns = ['Day', 'wRVUs']
-    wrvu_by_day_fig = px.bar(wrvu_by_day_src, x='Day', y='wRVUs', text='wRVUs', text_auto='.1f', hover_data={'wRVUs': ':.1f'})
+    wrvu_by_day_src = df.groupby("date").wrvu.sum().reset_index()
+    wrvu_by_day_src.columns = ["Day", "wRVUs"]
+    wrvu_by_day_fig = px.bar(wrvu_by_day_src, x="Day", y="wRVUs", text="wRVUs", text_auto=".1f", hover_data={"wRVUs": ":.1f"}).update_traces(marker_color="#2ca02c")
 
     # Display charts
-    main_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>Encounters</h4>", unsafe_allow_html=True)
-    main_ct.plotly_chart(encounters_by_month_fig, use_container_width=True)
-    main_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>wRVUs (by Month Posted)</h4>", unsafe_allow_html=True)
-    main_ct.plotly_chart(wrvu_by_month_fig, use_container_width=True)
+    enc_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">Encounters</h4>', unsafe_allow_html=True)
+    enc_ct.plotly_chart(encounters_by_month_fig, use_container_width=True)
+    rvu_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">wRVUs (by Month Posted)</h4>', unsafe_allow_html=True)
+    rvu_ct.plotly_chart(wrvu_by_month_fig, use_container_width=True)
 
-    quarter_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>Encounters by Quarter</h4>", unsafe_allow_html=True)
-    quarter_ct.plotly_chart(encounters_by_quarter_fig, use_container_width=True)
-    quarter_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>wRVUs by Quarter (Posted)</h4>", unsafe_allow_html=True)
-    quarter_ct.plotly_chart(wrvu_by_quarter_fig, use_container_width=True)
+    quarter_enc_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">Encounters by Quarter</h4>', unsafe_allow_html=True)
+    quarter_enc_ct.plotly_chart(encounters_by_quarter_fig, use_container_width=True)
+    quarter_rvu_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">wRVUs by Quarter (Posted)</h4>', unsafe_allow_html=True)
+    quarter_rvu_ct.plotly_chart(wrvu_by_quarter_fig, use_container_width=True)
 
-    daily_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>Encounters</h4>", unsafe_allow_html=True)
-    daily_ct.plotly_chart(encounters_by_day_fig, use_container_width=True)
-    daily_ct.markdown("<h4 style='text-align:center;padding-bottom:0px;'>wRVUs (by Visit Date)</h4>", unsafe_allow_html=True)
-    daily_ct.plotly_chart(wrvu_by_day_fig, use_container_width=True)
+    daily_enc_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">Encounters</h4>', unsafe_allow_html=True)
+    daily_enc_ct.plotly_chart(encounters_by_day_fig, use_container_width=True)
+    daily_rvu_ct.markdown('<h4 style="text-align:center;padding-bottom:0px;">wRVUs (by Visit Date)</h4>', unsafe_allow_html=True)
+    daily_rvu_ct.plotly_chart(wrvu_by_day_fig, use_container_width=True)
--- src/ui.py
+++ src/ui.py
@@ -4,8 +4,7 @@ import plotly.express as px
 import datetime as dt
 import arrow
 from datetime import date
-
-from . import auth, data, fig
+from . import auth, data, fig, dates
 
 def render_upload(cur_files: list = None):
     """Provide a way to upload updated data file"""
@@ -17,7 +16,7 @@ def render_upload(cur_files: list = None):
     files = st.file_uploader("Select files to upload", accept_multiple_files=True)
     return files, remove_existing
 
-def render_sidebar(data_start_date: date, data_end_date: date) -> tuple:
+def render_sidebar(data_start_date: date, data_end_date: date) -> tuple[str, date, date, date, date]:
     """Render widgets on sidebar for configuring dashboard"""
 
     start_date, end_date, compare_start_date, compare_end_date = None, None, None, None
@@ -32,42 +31,31 @@ def render_sidebar(data_start_date: date, data_end_date: date) -> tuple:
     )
 
     # Preset date filters
-    date_range = config_ct.selectbox("Dates:", ["This month", "Last month", "This year", "Last year", "This quarter", "Last quarter", "Specific dates"])
-    today = arrow.now().floor("day")
-    if date_range == "This month":
-        start_date = today.floor("month").date()
-        end_date = today.date()
-    elif date_range == "Last month":
-        start_date = today.floor("month").shift(months=-1).date()
-        end_date = today.floor("month").shift(days=-1).date() # one day prior to first day of this month
-    elif date_range == "This year":
-        start_date = today.floor("year").date()
-        end_date = today.date()
-    elif date_range == "Last year":
-        start_date = today.floor("year").shift(years=-1).date()
-        end_date = today.floor("year").shift(days=-1).date() # one day prior to Jan 1
-    elif date_range == "This quarter":
-        start_date = today.floor("quarter").date()
-        end_date = today.date()
-    elif date_range == "Last quarter":
-        start_date = today.floor("quarter").shift(quarters=-1).date()
-        end_date = today.floor("quarter").shift(days=-1).date() # one day prior to first day of this quarter
-    elif date_range == "Specific dates":
-        dates = config_ct.date_input("Select dates:", value=(data_start_date, today.date()))
-        if len(dates) > 1:
+    date_range = config_ct.selectbox("Dates:", ["This year", "This quarter", "This month", "Last year", "Last quarter", "Last month", "Specific dates"])
+    if date_range == "Specific dates":
+        specific_dates = config_ct.date_input("Select dates:", value=(data_start_date, date.today()))
+        if len(specific_dates) > 1:
             # Wait until both start and end dates selected to set date range
-            start_date, end_date = dates
+            start_date, end_date = specific_dates
     else:
-        start_date, end_date = None, None
+        start_date, end_date = dates.get_dates(date_range)
 
     # Option to compare to another date range
     compare_ct = config_ct.expander("Comparison Data")
     compare = compare_ct.checkbox("Show comparison display")
-    compare_date_range = compare_ct.selectbox("Date range:", ["Same days last month", "Same days last year", "Last month", "2 months ago", "Last year", "2 years ago", "Last quarter", "2 quarters ago", "3 quarters ago", "Specific dates"])
-    if date_range == "Specific dates":
-        compare_dates = compare_ct.date_input("Date range:", key="compare_dates", value=(data_start_date, today.date()))
+    compare_date_range = compare_ct.selectbox("Date range:", ["Same days 1 year ago", "Same days 1 month ago", "This year", "Last year", "2 years ago", "This quarter", "Last quarter", "2 quarters ago", "3 quarters ago", "This month", "Last month", "2 months ago", "Specific dates"])
+    if compare_date_range == "Specific dates":
+        compare_dates = compare_ct.date_input("Date range:", key="compare_dates", value=(data_start_date, date.today()))
         if len(compare_dates) > 1:
             compare_start_date, compare_end_date = compare_dates
+    elif compare_date_range == "Same days 1 month ago" and start_date is not None:
+        compare_start_date = arrow.get(start_date).shift(months=-1).date()
+        compare_end_date = arrow.get(end_date).shift(months=-1).date()
+    elif compare_date_range == "Same days 1 year ago" and start_date is not None:
+        compare_start_date = arrow.get(start_date).shift(years=-1).date()
+        compare_end_date = arrow.get(end_date).shift(years=-1).date()
+    else:
+        compare_start_date, compare_end_date = dates.get_dates(compare_date_range)
     if not compare:
         # Do not perform comparison if enable box is unchecked, so clear dates
         compare_start_date, compare_end_date = None, None
@@ -81,7 +69,7 @@ def render_main(data: data.FilteredRvuData, compare: data.FilteredRvuData) -> No
         st.markdown("<h5 style='color:#6e6e6e; padding-top:65px;'>Select a provider and date range</h5>", unsafe_allow_html=True)
         return
 
-    df, partitions, stats = data.df, data.partitions, data.stats
+    df, partitions, stats, = data.df, data.partitions, data.stats
     cmp_df, cmp_partitions, cmp_stats = (compare.df, compare.partitions, compare.stats) if compare is not None else (None, None, None)
 
     # Summary stats including overall # patients and wRVUs
@@ -89,19 +77,21 @@ def render_main(data: data.FilteredRvuData, compare: data.FilteredRvuData) -> No
     if compare is None:
         dates_ct = st.empty()
         ct1, ct2, ct3 = st.columns(3)
-        fig.st_summary(stats, dates_ct, ct1, ct2, ct3)
+        fig.st_summary(stats, data.start_date, data.end_date, dates_ct, ct1, ct2, ct3)
     else:
         # Write metrics in side-by-side vertical columns
         colL, colR = st.columns(2)
-        fig.st_summary(stats, colL, colL, colL, colL)
-        fig.st_summary(cmp_stats, colR, colR, colR, colR)
+        fig.st_summary(stats, data.start_date, data.end_date, colL, colL, colL, colL)
+        fig.st_summary(cmp_stats, compare.start_date, compare.end_date, colR, colR, colR, colR)
 
     # Summary graphs
     if compare is None:
-        main_ct = st.container()
+        enc_ct, rvu_ct = st.columns(2)
         quarter_ct = st.expander("By Quarter")
+        quarter_enc_ct, quarter_rvu_ct = quarter_ct.columns(2)
         daily_ct = st.expander("By Day")
-        fig.st_summary_figs(df, partitions, main_ct, quarter_ct, daily_ct)
+        daily_enc_ct, daily_rvu_ct = daily_ct.columns(2)
+        fig.st_summary_figs(df, partitions, enc_ct, rvu_ct, quarter_enc_ct, quarter_rvu_ct, daily_enc_ct, daily_rvu_ct)
     else:
         main_ct = st.container()
         main_colL, main_colR = main_ct.columns(2)
@@ -109,5 +99,5 @@ def render_main(data: data.FilteredRvuData, compare: data.FilteredRvuData) -> No
         quarter_colL, quarter_colR = quarter_ct.columns(2)
         daily_ct = st.expander("By Day")
         daily_colL, daily_colR = daily_ct.columns(2)
-        fig.st_summary_figs(df, partitions, main_colL, quarter_colL, daily_colL)
-        fig.st_summary_figs(cmp_df, cmp_partitions, main_colR, quarter_colR, daily_colR)
+        fig.st_summary_figs(df, partitions, main_colL, main_colL, quarter_colL, quarter_colL, daily_colL, daily_colL)
+        fig.st_summary_figs(cmp_df, cmp_partitions, main_colR, main_colR, quarter_colR, quarter_colR, daily_colR, daily_colR)
