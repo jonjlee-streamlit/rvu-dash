53afac62f1d94f59fefb8abd061e5c80a70cc3da
Jonathan Lee <jonjlee@gmail.com>
2023-06-15 17:00:40 -0700

    Get files from env var instead of st.secrets

--- src/data_files.py
+++ src/data_files.py
@@ -1,19 +1,21 @@
 import os
-import streamlit as st
 
 # Location of data files: rvu-dash/data/
 BASE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data")
 
+
 def get():
     """Return list of data files. Defaults to 'files' config var if set, otherwise list of local files"""
-    return st.secrets.get("files") or get_local()
+    return os.environ.get("STREAMLIT_DATA_FILES") or get_local()
+
 
 def get_local():
     """Return list of local data files"""
     if not os.path.isdir(BASE_PATH):
         return []
-    
+
     return [os.path.join(BASE_PATH, local) for local in os.listdir(BASE_PATH)]
+
 
 def update_local(files, remove_existing):
     if files is None or len(files) == 0:
@@ -21,12 +23,12 @@ def update_local(files, remove_existing):
 
     # Ensure base data directory exists
     os.makedirs(BASE_PATH, exist_ok=True)
-    
+
     # Delete all files if requested
     if remove_existing:
         for local in os.listdir(BASE_PATH):
             os.remove(os.path.join(BASE_PATH, local))
-    
+
     # Save new files to data dir
     for file in files:
         with open(os.path.join(BASE_PATH, file.name), "wb") as local:
