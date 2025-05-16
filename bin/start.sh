3b684c23af368c78d4b1ece4ebcb14aadd1efc1d
Jonathan Lee <jonjlee@gmail.com>
2025-05-16 16:52:17 -0700

    Update start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,25 @@
-# Start streamlit in virtual environment. 
-# To run interactively without script, be sure to first: pipenv shell
-# Disable CORS and Xsfr protection per https://github.com/orgs/community/discussions/18038
-pipenv run streamlit run app.py --server.enableCORS false --server.enableXsrfProtection false
+#!/bin/bash
+
+INTERACTIVE=false
+
+# Parse options
+while getopts "i" opt; do
+  case $opt in
+    i)
+      INTERACTIVE=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$INTERACTIVE" = true ]; then
+    # Interactive mode
+    pipenv run streamlit run app.py
+else
+    # Background mode
+    nohup pipenv run streamlit run app.py &> nohup.out & echo $! > streamlit.pid
+    echo "Dashboard started in background. PID saved to streamlit.pid"
+fi
